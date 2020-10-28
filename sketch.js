25172e18a498c059f2a6d8fd77aad2e5207f14fc
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2020-10-28 22:37:55 +0200

    Invert colors

--- sketch.js
+++ sketch.js
@@ -7,18 +7,17 @@ class CanvasDrawer{
         this.canvas.height = canvas_size[1];
         this.ctx = canvas.getContext("2d");
         this.cursorPos = new Vec(0, 0);
-        this.strokeEdges();
         this.canvas.addEventListener("mousemove", (evt) => this.calcCursorPos(evt)); //This is dumb. ("mousemove", this.calcCursorPos) doesn't work since "this" in this.calcCursorPos refers to the canvas???
     }
 
     strokeEdges(){
-        this.ctx.strokeStyle = "#000000";
+        this.ctx.strokeStyle = "#FFFFFF";
         this.ctx.lineWidth = 1;
         this.ctx.globalAlpha = 1;
         this.ctx.strokeRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
-    line(p1, p2, strokeStyle = "#000000", alpha = 1.0, width = 1){
+    line(p1, p2, strokeStyle = "#FFFFFF", alpha = 1.0, width = 1){
         this.ctx.strokeStyle = strokeStyle;
         this.ctx.globalAlpha = alpha;
         this.ctx.lineWidth = Math.ceil(width);
@@ -34,10 +33,12 @@ class CanvasDrawer{
     clear(){
         this.ctx.beginPath();
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.strokeEdges();
+        //this.strokeEdges();
     }
 
     refresh(){
+        this.ctx.fillStyle = "#000000"
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.stroke();
         this.ctx.fill();
     }
